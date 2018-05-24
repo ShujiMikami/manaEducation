b34c9aefb21557a11a4bd987f43d120e54506bc9
DMJ\MM07860 <shuji_mikami@murata.com>
2018-05-24 22:07:53 +0900

    BMI問題追加

--- src/test.cpp
+++ src/test.cpp
@@ -9,44 +9,17 @@
 #include <iostream>
 using namespace std;
 
-void seikai()
-{
-	static int x = 0;
-	int y;
-	for(int i = 0; i < 5; i++){
-		cout << "数字を入力してください, 入力した数字を前の結果に足します" << endl;
-		cin >> y;
-
-		x += y;
-		cout << x << endl;
-	}
-
-}
-
 
 int main() {
-	/*
-	 * 以下のプログラムは, 3を2で割る計算結果を出力するプログラムである
-	 */
-	double x = 1;
-	double y = 3;
-	double z = 0;
 
-	z = x / y;
-	cout << z << endl; //答えは1が出力される.
 
 	/*問題
-	 * 以上のプログラムにおける, "int"をすべて, "double"に書き換えて, 答えがどうなるか確認しなさい
+	 * ユーザーに体重と, 身長を, 小数点で, 入力させ,
+	 * (体重の2乗)/(身長)を計算して出力するプログラムをつくりなさい.
+	 * 体重を入力させる際, "体重を入力してください"と文字を出力し,
+	 * 身長を入力させる際, "身長を入力してください"と文字を出力し,
+	 * 結果を出力する際, "BMI=XX"と出力させなさい.
 	 */
-
-	double hankei;
-	cin>>hankei;
-	double pai;
-	pai=3.14;
-	double menseki;
-	menseki=hankei*hankei*pai;
-	cout<<menseki<<endl;
-
 
 
 
