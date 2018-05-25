72f2f09ebcd0e4db288f5a2752c93fa7db51a59d
DMJ\MM07860 <shuji_mikami@murata.com>
2018-05-25 17:08:36 +0900

    mondai

--- src/test.cpp
+++ src/test.cpp
@@ -11,36 +11,22 @@ using namespace std;
 
 
 int main() {
-	/*問題
+	/*問題1
 	 *
 	 * ある小数aが最初100である
 	 * ユーザーに小数b, 小数cを入力させ, aにbとcをかける
-	 * これを5回繰り返したあとのaの値を出力するプログラムを作りなさい
+	 * aの結果を出力する
+	 * これを5回繰り返したあと,　ユーザーに小数dを入力させる
+	 * 最後にa/dを出力するプログラムを作りなさい
 	 */
-	double a=100;
-	double b=0;
-	double c=0;
 
-	cin>>b;
-	cin>>c;
-	a=a*b*c;
-	cout<<a<<endl;
-	cin>>b;
-		cin>>c;
-		a=a*b*c;
-		cout<<a<<endl;
-		cin>>b;
-			cin>>c;
-			a=a*b*c;
-			cout<<a<<endl;
-			cin>>b;
-				cin>>c;
-				a=a*b*c;
-				cout<<a<<endl;
-				cin>>b;
-					cin>>c;
-					a=a*b*c;
-					cout<<a<<endl;
+	/*問題2
+	 * ある小数xが最初10である
+	 * ある少数yが最初20である
+	 * ユーザーに小数z,小数wを入力させ, xにzをかけ, yにwをかける
+	 * x, yを出力する
+	 * これを5回繰り返した後, 最後に, x + yを出力するプログラムを作りなさい
+	 */
 
 
 	return 0;
